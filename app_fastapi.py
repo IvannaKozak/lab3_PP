e909ed928bde21e07eebb1d6cc1080a9f3446800
ivanka <ivanka.kozak3@gmail.com>
2023-11-20 14:32:53 +0200

    added instructions to readme.md

--- app_fastapi.py
+++ app_fastapi.py
@@ -4,5 +4,5 @@ from starlette import status
 app = FastAPI()
 
 @app.get("/api/v1/hello-world-16", status_code=status.HTTP_200_OK)
-def read_root():
+def hello_world():
     return {"message": "Hello World 16"}
